276d927feeec86caac497f395a12e8b93294cf2d
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2025-04-06 12:22:21 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -2,10 +2,14 @@ import requests
 import time
 import json
 i=0
+ranfor=0
+sttime=time.time()
 while(True):
   i+=1
   st = time.time()
-  r = requests.patch('https://jqualin-990c1-default-rtdb.firebaseio.com/r.json',json={'i':i,'time':time.ctime()})
-  print(time.time()-st)
+  ranfor=time.time()-sttime
+  r = requests.patch('https://jqualin-990c1-default-rtdb.firebaseio.com/r.json',json={'i':i,'time':time.ctime(),'ranfor':ranfor})
+  #print(time.time()-st)
+  if ranfor>=10600:
+    break
   time.sleep(3)
-time.sleep(10)
