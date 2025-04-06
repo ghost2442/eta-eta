582942877697f9732b23246469eb5d989675bdb0
ghost2442 <68842877+ghost2442@users.noreply.github.com>
2025-04-06 12:17:06 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,6 +1,11 @@
 import requests
 import time
 import json
-r = requests.patch('https://jqualin-990c1-default-rtdb.firebaseio.com/r.json',json={'hi':'mf'})
-print(r)
+i=0
+while(True):
+  i+=1
+  st = time.time()
+  r = requests.patch('https://jqualin-990c1-default-rtdb.firebaseio.com/r.json',json={'i':i,'time':time.ctime()})
+  print(time.time()-st)
+  time.sleep(3)
 time.sleep(10)
